1c3d93858618c73dc477df9e9e1dd67f551b1e41
swkeny <swk.kenyon@gmail.com>
2016-12-07 22:56:17 -0500

    cleanup of outboundOrdersSet creation in script, and added time suffix to filenames

--- OrderCorrelationRoutine.py
+++ OrderCorrelationRoutine.py
@@ -3,8 +3,10 @@ import os
 import time
 
 # ideally all cache server files would be in one folder, and all loader files in another folder
-outboundOrderPath = ''
-inboundOrderPath = ''
+# outboundOrderPath = ''
+# inboundOrderPath = ''
+outboundOrderPath = 'C:/Users/deifen/PycharmProjects/OrderCorrelation/order-time-analysis/Data/OutgoingOrders'
+inboundOrderPath = 'C:/Users/deifen/PycharmProjects/OrderCorrelation/order-time-analysis/Data/IncomingOrders'
 outboundOrdersSet = {}
 inboundOrdersSet = {}
 
@@ -16,7 +18,12 @@ for filename in os.listdir(outboundOrderPath):
     order = {}
     for child in root:
         # collect all the attributes common to the whole file. Still Need to exclude Order and add create time from filename.....
-        order[child.tag] = child.text
+        if child.tag == "portId":
+            portId = child.text
+        if child.tag == "typ":
+            type = child.text
+        order["typ"] = type
+        order["portId"] = portId
 
     orders = tree.findall(".//Order")
     for o in orders:
@@ -26,8 +33,8 @@ for filename in os.listdir(outboundOrderPath):
                 allocId = child.text
             if child.tag == "status":
                 status = child.text
-        order['status'] = status
-        order['timestamp'] = filename[-13:filename.find(".xml")]
+        order["status"] = status
+        order["timestamp"] = filename[-13:filename.find(".xml")]
         # Add the order to the order set
         outboundOrdersSet[allocId] = order
 
@@ -38,7 +45,12 @@ for filename in os.listdir(inboundOrderPath):
     order = {}
     for child in root:
         # collect all the attributes common to the whole file. Still Need to exclude Order and add create time from filename.....
-        order[child.tag] = child.text
+        if child.tag == "portId":
+            portId = child.text
+        if child.tag == "typ":
+            type = child.text
+        order["typ"] = type
+        order["portId"] = portId
 
     orders = tree.findall(".//Order")
     for o in orders:
@@ -50,8 +62,8 @@ for filename in os.listdir(inboundOrderPath):
             if child.tag == "status":
                 status = child.text
         if status == PENDING_STATUS_FILTER:
-            order['status'] = status
-            order['timestamp'] = filename[-13:filename.find(".xml")]
+            order["status"] = status
+            order["timestamp"] = filename[-13:filename.find(".xml")]
             # Add the order to the order set
             inboundOrdersSet[allocId] = order
 

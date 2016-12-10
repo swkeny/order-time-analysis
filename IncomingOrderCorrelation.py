a7b1d7915413ed089618aa8cb86aae6be46f387d
swkeny <swk.kenyon@gmail.com>
2016-12-10 01:20:26 -0500

    refining both incoming and outgoing scripts, and removing nasty side effect from reusing the same instance of the order dictionary

--- IncomingOrderCorrelation.py
+++ IncomingOrderCorrelation.py
@@ -1,66 +1,80 @@
+from collections import OrderedDict
 import xml.etree.ElementTree as ET
 import os
-import csv
 
-# ideally all cache server files would be in one folder, and all loader files in another folder
+# ideally all loader files would be in one folder, and all loader files in another folder
 inboundOrderPath = 'C:/Users/deifen/PycharmProjects/OrderCorrelation/order-time-analysis/Data/IncomingOrders'
-
 inboundOrdersSet = {}
-PENDING_STATUS_FILTER = "PENDING"
 inboundOrderFileCount = 0
 inboundOrdersFiltered = 0
+inboundOrdersTotal = 0
+allocationCount = 0
+fileCount = 0
+PENDING_STATUS_FILTER = "PENDING"
+
+print("Building Inbound Orders set...")
+
+# loop through each file in the specified path
 for filename in os.listdir(inboundOrderPath):
+    fileCount += 1
     inboundOrderFileCount += 1
     tree = ET.parse(os.path.join(inboundOrderPath, filename))
     root = tree.getroot()
-    # print(filename)
-    order = {}
+# strip time elements from filename, and convert to seconds elapsed since midnight
+    timestamp = filename[-13:filename.find(".xml")]
+    hours = timestamp[0:2]
+    minutes = timestamp[2:4]
+    seconds = timestamp[4:6]
+    subseconds = timestamp[6:]
+    timeInSeconds = float(str(int(hours) * 60 * 60 + int(minutes) * 60 + int(seconds)) + "." + subseconds)
+# collect all the attributes common to the whole file
     for child in root:
-        # collect all the attributes common to the whole file. Still Need to exclude Order and add create time from filename.....
         if child.tag == "portId":
             portId = child.text
         if child.tag == "typ":
             type = child.text
-        order["typ"] = type
-        order["portId"] = portId
-
+# find all Order elements in the xml file, and loop through to extract the attributes of each order
     orders = tree.findall(".//Order")
     for o in orders:
-
+        order = OrderedDict()
+        inboundOrdersTotal += 1
         for child in o:
-            # Find the attributes of each order in the order list
             if child.tag == "allocId":
                 allocId = child.text
             if child.tag == "status":
                 status = child.text
+# if the order status is PENDING, then build the order and add it to the order set
         if status == PENDING_STATUS_FILTER:
+            allocationCount += 1
+            order["allocId"] = allocId
+            order["typ"] = type
             order["status"] = status
-            timestamp = filename[-13:filename.find(".xml")]
-            hours = timestamp[0:2]
-            minutes = timestamp[2:4]
-            seconds = timestamp[4:6]
-            subseconds = timestamp[6:]
-            timeInSeconds = float(str(int(hours) * 60 * 60 + int(minutes) * 60 + int(seconds)) + "." + subseconds)
+            order["portId"] = portId
             order["timeInSeconds"] = timeInSeconds
-            # Add the order to the order set
             inboundOrdersSet[allocId] = order
+# keep track of how many orders were filtered with status <> PENDING
         else:
             inboundOrdersFiltered += 1
 
-columnHeaders = "COLUMN1, COLUMN2, COLUMN3, COLUMN4, COLUMN5"
+print("Inbound Orders set complete")
+print("Saving to disk...")
+
+columnHeaders = "AllocID, MessageType, Status, PortID, SecondsFromMidnight"
 inboundOrders = [columnHeaders]
 
-for o in inboundOrdersSet:
+# loop through order set and prepare for output to file by packaging as a comma delimited string
+
+for o in inboundOrdersSet.keys():
     tString = ""
     k = 1
-    for i in inboundOrdersSet[o]:
+    for i in inboundOrdersSet[o].keys():
+# lets not add a comma after the last element in the set......
         if k == len(inboundOrdersSet[o]):
             tString += (str(inboundOrdersSet[o][i]))
         else:
             tString += (str(inboundOrdersSet[o][i])) + ", "
         k += 1
-    inboundOutputString = o + ", " + tString
-    inboundOrders.append(inboundOutputString)
+    inboundOrders.append(tString)
 
 f2 = open('inboundOrders.txt', 'w')
 
@@ -68,3 +82,8 @@ for line in inboundOrders:
   f2.write("%s\n" % line)
 
 f2.close()
+
+# print run stats
+print("Save complete")
+print("Found " + str(allocationCount) + " allocations in " + str(fileCount) + " files")
+print("Filtered " + str(inboundOrdersFiltered) + " orders out of a total of " + str(inboundOrdersTotal))
--- OutgoingOrderCorrelation.py
+++ OutgoingOrderCorrelation.py
@@ -1,49 +1,47 @@
 from collections import OrderedDict
 import xml.etree.ElementTree as ET
 import os
-import csv
 
 # ideally all cache server files would be in one folder, and all loader files in another folder
 outboundOrderPath = 'C:/Users/deifen/PycharmProjects/OrderCorrelation/order-time-analysis/Data/OutgoingOrders'
 outboundOrdersSet = {}
 outboundOrderFileCount = 0
-allocationCoumt = 0
+allocationCount = 0
 fileCount = 0
 
 print("Building Outbound Orders set...")
 
-#Loop through each file in the specified path
+# loop through each file in the specified path
 for filename in os.listdir(outboundOrderPath):
     fileCount += 1
     outboundOrderFileCount += 1
     tree = ET.parse(os.path.join(outboundOrderPath, filename))
     root = tree.getroot()
-    # order = {}
-    order = OrderedDict()
-#strip time elements from filename, and convert to seconds elapsed since midnight
+# strip time elements from filename, and convert to seconds elapsed since midnight
     timestamp = filename[-13:filename.find(".xml")]
     hours = timestamp[0:2]
     minutes = timestamp[2:4]
     seconds = timestamp[4:6]
     subseconds = timestamp[6:]
     timeInSeconds = float(str(int(hours) * 60 * 60 + int(minutes) * 60 + int(seconds)) + "." + subseconds)
-#collect all the attributes common to the whole file
+# collect all the attributes common to the whole file
     for child in root:
         if child.tag == "progId":
             progId = child.text
-#find all Order elements in the xml file, loop through them looking for Allocation elements
+# find all Order elements in the xml file, and loop through them looking for Allocation elements
     orders = tree.findall(".//Order")
     for o in orders:
         for child in o:
             if child.tag == "Allocation":
-# Find the attributes of each allocation under the current order
-                allocationCoumt += 1
+                order = OrderedDict()
+                allocationCount += 1
+# find the attributes of each allocation under the current order
                 for a in child.getchildren():
                     if a.tag == "allocId":
                         allocId = a.text
                     if a.tag == "portId":
                         portId = a.text
-#build the order, and add it to the order set
+# build the order, and add it to the order set
                 order["allocId"] = allocId
                 order["progId"] = progId
                 order["portId"] = portId
@@ -56,12 +54,12 @@ print("Saving to disk...")
 columnHeaders = "AllocID, ProgID, PortID, SecondsFromMidnight"
 outboundOrders = [columnHeaders]
 
-#loop through order set and prepare for output to file by packaging as a comma delimited string
+# loop through order set and prepare for output to file by packaging as a comma delimited string
 for o in outboundOrdersSet:
     tString = ""
     k = 1
     for i in outboundOrdersSet[o]:
-#lets not add a comma after the last element in the set......
+# lets not add a comma after the last element in the set......
         if k == len(outboundOrdersSet[o]):
             tString += (str(outboundOrdersSet[o][i]))
         else:
@@ -76,9 +74,9 @@ for line in outboundOrders:
 
 f1.close()
 
-#print run stats
+# print run stats
 print("Save complete")
-print("Found " + str(allocationCoumt) + " allocations in " + str(fileCount) + " files")
+print("Found " + str(allocationCount) + " allocations in " + str(fileCount) + " files")
 
 
 
